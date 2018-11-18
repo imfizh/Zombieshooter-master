a2649aac3de4631cb1844c5b78ad0e36bbac91ce
imfizh <43548110+imfizh@users.noreply.github.com>
2018-11-18 18:52:19 +0000

    start screen

---
+++ Assets/script/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("zombie shooter");
+    }
+    public void EndGame()
+    {
+        SceneManager.LoadScene("end game");
+    }
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene("start screen");
+    }
+}
